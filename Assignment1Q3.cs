ec4b50e5b3334610f74dfcbcc47c0a9f71a79451
Keerthi Reddy <115027344+Keerthireddy369@users.noreply.github.com>
2022-11-12 17:12:48 +0000

    Assignment 1 class files

---
+++ Assignment1Q3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Assignment1Q3
+    {
+        static void Main(string[] args)
+        {
+            Assignment1Q3.Sum1(8, 6, 5, 4, 3);
+        }
+        public static void Sum1(params int[] number)
+        {
+
+            int result = number[0] + number[1] + number[2] + number[3] + number[4];
+            Console.WriteLine("Sum of the numbers {0}", result);
+            Console.ReadKey();
+
+
+        }
+    }
+}
