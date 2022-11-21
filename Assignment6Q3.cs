e6b48ba32c40a52b5efe1807c08062e8b2bafa83
Keerthi Reddy <115027344+Keerthireddy369@users.noreply.github.com>
2022-11-21 03:59:32 +0000

    Assignment6 sucess

---
+++ Assignment6Q3.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+
+
+    class BankAccount
+    {
+        public double AccountNumber = 12345678;
+        public String Name = "Indra";
+        public double bankbalance = 133333434;
+        public void data()
+        {
+            Console.WriteLine("Accountnumber={0}\nAccountHolder={1}\nBankbalace={2}", AccountNumber, Name, bankbalance);
+        }
+        public void deposit(double money)
+        {
+            if (money > 0)
+            {
+                bankbalance = bankbalance + money;
+                Console.WriteLine("Bank Balance=" + bankbalance);
+
+            }
+            else
+            {
+                Console.WriteLine("enter correct value of amount");
+
+            }
+            Console.WriteLine("Bank Balance=" + bankbalance);
+        }
+        public void Withdraw(double amount)
+        {
+            if (amount < bankbalance)
+            {
+                bankbalance -= amount;
+            }
+            else if (amount > bankbalance)
+            {
+                Console.WriteLine("Insufficient Balance");
+            }
+            else
+            {
+                Console.WriteLine("select correct choice");
+            }
+            Console.WriteLine("Bank Balance=" + bankbalance);
+
+
+
+        }
+        public void balance()
+        {
+            Console.WriteLine("bank balance=" + bankbalance);
+        }
+
+
+
+    }
+    class ICICIBankAccount : BankAccount
+    {
+        public void withdraw(double amount)
+        {
+            if (amount < bankbalance)
+            {
+                bankbalance -= amount;
+            }
+
+
+
+
+        }
+    }
+}
+
